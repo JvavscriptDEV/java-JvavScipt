1c14ced9b64a446aef2312bbd6d162893a242a59
The Artificial Intelligence <64889395+ChengT-cmd@users.noreply.github.com>
2021-03-11 19:55:15 +0800

    Add files via upload

--- src/main/JvavScriptmain.java
+++ src/main/JvavScriptmain.java
@@ -1,40 +1,43 @@
 package main;
-import java.util.Scanner;
+import java.util.*;
 
-@Overview
 public class JvavScriptmain {
 	public static void main(String[] agrs) {
-		System.out.println("================JVAVSCRIPT================");
-		System.out.println("|            Joked BY Dr.HY_Z            |");
-		System.out.println("|        Powered by JvavScript Team      |");
-		System.out.println("|Enter 1 to get characters for processing|");
-		System.out.println("|       Enter 2 output characters        |");
-		System.out.println("==========================================");
+		System.out.println("===================JVAVSCRIPT=================");
+		System.out.println("|            Joked BY Dr.HY_Z                |");
+		System.out.println("|        Powered by JvavScript Team          |");
+		System.out.println("|Enter input to get characters for processing|");
+		System.out.println("|       Enter output output characters       |");
+		System.out.println("==============================================");
 		System.out.print("JvavScript>");
-		Scanner input=new Scanner(System.in);
-		int x=0,y=0;
-		x=input.nextInt();
-		switch(x) {
-			case 1:
-				Scanner in=new Scanner(System.in);
-				System.out.print("Inputing>");
-				y=in.nextInt();
-				System.out.println("---------------------------------------------\n");
-				System.out.println("Program exited with return value 0\n");
-				break;
-			case 2:
-				System.out.print("Enter the word you want output>");
-				Scanner ou=new Scanner(System.in);
-				int zz=ou.nextInt();
-				System.out.println("---------------------------------------------\n");
-				System.out.println("Program exited with return value 3221225477\n");
-				break;
-			default:
-				System.out.println("ERROR!!!Unknow value");
-				System.out.println("---------------------------------------------\n");
-				System.out.println("Program exited with return value 1\n");
-				break;
+		Scanner in = new Scanner(System.in);
+		String temp;
+		while(true)
+		{
+			temp = in.nextLine();
+			if(temp.equals("input"))
+			{
+					System.out.print("Inputing>");
+					temp=in.nextLine();
+			}
+			else if(temp.equals("output"))
+			{
+					System.out.print("Enter the word you want output>");
+					temp=in.nextLine();
+					System.out.println(temp);
+			}
+			else if(temp.equals("exit") || temp.equals("exit()"))
+			{
+				return;
+			}
+			else
+			{
+					System.out.println("ERROR : unknown command!");
+			}
+			System.out.print("JvavScript>");
 		}
+		/// this is a easy java program
+		/// and it is ~~reeeeeeeeeeeeeeeally useless~~
+		/// \_(ツ)_/
 	}
-
 }

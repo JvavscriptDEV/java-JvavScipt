819ea890afb14679795a5a015f7eb45326ad6967
The Artificial Intelligence <64889395+ChengT-cmd@users.noreply.github.com>
2021-03-12 20:40:34 +0800

    Update JvavScriptmain.java

--- src/main/JvavScriptmain.java
+++ src/main/JvavScriptmain.java
@@ -2,12 +2,19 @@ package main;
 import java.util.*;
 
 public class JvavScriptmain {
+	public static void cout(String whattooutput)
+	{
+		System.out.print(whattooutput);
+		return;
+	}
 	public static void main(String[] agrs) {
+		String version = "2.0.2";
 		System.out.println("===================JVAVSCRIPT=================");
+		System.out.println("|                                            |");
 		System.out.println("|            Joked BY Dr.HY_Z                |");
-		System.out.println("|        Powered by JvavScript Team          |");
-		System.out.println("|Enter input to get characters for processing|");
-		System.out.println("|       Enter output output characters       |");
+		System.out.println("|         JvavScript Java 2021 (v2)          |");
+		System.out.println("|          Type \"help\" for help            |");
+		System.out.println("|                                            |");
 		System.out.println("==============================================");
 		System.out.print("JvavScript>");
 		Scanner in = new Scanner(System.in);
@@ -15,7 +22,15 @@ public class JvavScriptmain {
 		while(true)
 		{
 			temp = in.nextLine();
-			if(temp.equals("input"))
+			if(temp.equals("help"))
+			{
+				System.out.println("JvavScript Java Help:\n");
+				cout("version\t:\toutput version of Java-Jvavscript.\n");
+				cout("input\t:\tinput a string for nothing.\n");
+				cout("output\t:\toutput a strong.\n");
+				cout("exit or exit()\t:\texit.\n");
+			}
+			else if(temp.equals("input"))
 			{
 					System.out.print("Inputing>");
 					temp=in.nextLine();
@@ -30,12 +45,16 @@ public class JvavScriptmain {
 			{
 				return;
 			}
+			else if(temp.equals("version"))
+			{
+				System.out.println(version);
+			}
 			else
 			{
 					System.out.println("ERROR : unknown command!");
 			}
 			System.out.print("JvavScript>");
 		}
-		// \_(ツ)_/
+		/// \_(ツ)_/
 	}
 }
